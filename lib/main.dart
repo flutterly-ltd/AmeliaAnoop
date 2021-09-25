1aa87633558d7d65965f01851c3f5fe60fc1e2db
Anoop Jose <anoop@flutterly.co.uk>
2021-09-25 15:26:13 +0100

    reworking the entire project from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,52 @@
-import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
-import 'app.dart';
-import 'model/app_state_model.dart';
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+import 'widgets/amelia_avatar.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider<AppStateModel>(
-      // NEW
-      create: (_) => AppStateModel()..loadEvents(),
-      child: const EventsApp()));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashPage(
+      duration: 3,
+      goToPage: const WelcomePage(),
+    ),
+  ));
+}
+
+class SplashPage extends StatelessWidget {
+  SplashPage({Key? key, this.duration, this.goToPage}) : super(key: key);
+
+  int? duration;
+  Widget? goToPage;
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: duration!), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => goToPage!));
+    });
+    return Scaffold(
+        body: Container(
+      color: Colors.white,
+      alignment: Alignment.center,
+      child: AmeliaAvatar(
+        size: 200,
+      ),
+    ));
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: const Text("Hello world!"),
+      ),
+    );
+  }
 }
